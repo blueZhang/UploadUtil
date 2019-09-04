fdbd1c562eea315b4c644d4acfffb8bfbd8c882d
zhangfan <zhangfan@icaipiao123.com>
2019-09-04 18:55:48 +0800

    add class

---
+++ app/src/main/java/example/blue/example/shelllib/RootClass.java
@@ -0,0 +1,11 @@
+package example.blue.example.shelllib;
+
+/**
+ * Created by blueZhang on 2019-09-04.
+ *
+ * @Author: BlueZhang
+ * @date: 2019-09-04
+ */
+public class RootClass {
+
+}
---
+++ upload/src/main/java/example/blue/example/upload/UploadClass.java
@@ -0,0 +1,11 @@
+package example.blue.example.upload;
+
+/**
+ * Created by blueZhang on 2019-09-04.
+ *
+ * @Author: BlueZhang
+ * @date: 2019-09-04
+ */
+public class UploadClass {
+
+}
